7b21ee7c23f9408490c7f5884154395809fc688c
Dave Woodall <dave.woodall@livingsocial.com>
2014-07-29 19:34:00 -0600

    write a simple test

---
+++ spec/string_util_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require './string_util'
+
+RSpec.describe String do
+ describe 'iterm colors' do
+   it 'returns bash code for cyan ' do
+     string = 'this is my string'
+     expect(string.cyan).to eq("\e[36mthis is my string\e[0m")
+   end
+ end
+end
+
