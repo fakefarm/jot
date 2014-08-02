dcc83a460a07d2db3a3eacc77e35ad331c446778
woodall <dave@woodalls.me>
2014-08-02 11:32:41 -0600

    begin specs for preferences

---
+++ spec/preferences_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require './lib/preferences'
+
+RSpec.describe Preferences do
+  it 'saving destination'
+  it 'single or multiple files'
+  # will need to review how to mock out gets and terminal entries
+end
---
+++ spec/settings_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require './lib/settings'
+
+RSpec.describe Settings do
+  it 'checks the configs' 
+  it 'if jot is new, it will initiate a setup wizard'
+  it 'if jot is old, it will use the established settings from setup wizard'
+end
