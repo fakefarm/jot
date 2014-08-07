bd32dbff1af39ae420bf6157ee7493ed6949b614
woodall <dave@woodalls.me>
2014-08-07 06:24:13 -0600

    start over with settings

--- lib/settings.rb
+++ lib/settings.rb
@@ -2,47 +2,58 @@ require 'yaml'
 require 'setup_wizard'
 
 class Settings
-  attr_accessor :boot_file
 
-  def initialize
-    load_boot_file
-  end
-
-  def save_location 
-    boot_file_save_location 
-  end
-
-  def file_storage
-    boot_file_storage
+  def directory
+    get_settings_file
   end
 
 private
 
-  def new_account?
-    File.exists?('../config/boot.yaml')
-  end
-  
-  def load_boot_file
-    if new_account?
-      @boot_file = YAML.load_file('../config/boot.yaml')
-    else
-      setup_wizard
-    end
+  def get_settings_file
+    '/Dropbox/jots'
   end
 
-  def boot_file_save_location
-    boot_file['settings']['save_location']
-  end
-  
-  def boot_file_storage
-    boot_file['settings']['file_storage']
-  end
-
-  def setup_wizard
-    SetupWizard.new
-  end 
-
-  def status
-    boot_file['settings']['status']
-  end
+#   attr_accessor :boot_file
+# 
+#   def initialize
+#     load_boot_file
+#   end
+# 
+#   def save_location 
+#     boot_file_save_location 
+#   end
+# 
+#   def file_storage
+#     boot_file_storage
+#   end
+# 
+# private
+# 
+#   def new_account?
+#     File.exists?('../config/boot.yaml')
+#   end
+#   
+#   def load_boot_file
+#     if new_account?
+#       @boot_file = YAML.load_file('../config/boot.yaml')
+#     else
+#       setup_wizard
+#     end
+#   end
+# 
+#   def boot_file_save_location
+#     boot_file['settings']['save_location']
+#   end
+#   
+#   def boot_file_storage
+#     boot_file['settings']['file_storage']
+#   end
+# 
+#   def setup_wizard
+#     SetupWizard.new
+#   end 
+# 
+#   def status
+#     boot_file['settings']['status']
+#   end
 end
--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -2,36 +2,8 @@ require 'spec_helper'
 require 'settings'
 
 RSpec.describe Settings do
-  it 'if jot is new, it will initiate a setup wizard' do
+  it 'locates directory of stored jots' do
     settings = Settings.new
-    allow(settings).to receive(setup_wizard).and_return('')
-    expect(settings.new_account?).to eq true
-  end
-
-  it 'if existing jot, it will not initiate setup wizard' do
-    settings = Settings.new
-    allow(settings).to receive(:new_account?).and_return false
-    expect(settings.setup_wizard).not.to have_been_called
-  end
-
-  it 'locates directory for storage' do
-    settings = Settings.new
-    #TODO 
-    # Is it okay if I mockout the private methods from the object under test?
-    allow(settings).to receive(:boot_file_save_location).and_return('/Desktop')
-    expect(settings.save_location).to eq('/Desktop')
-  end
-
-  it 'shows single or multiple file preference' do
-    settings = Settings.new
-    allow(settings).to receive(:boot_file_storage).and_return('multiple')
-    expect(settings.file_storage).to eq('multiple')
-  end
-
-  xit 'can update file_location' do
-    settings = Settings.new
-    # TODO how do I mock out fake test yml file?
-    settings.update_file_location = '/Dropbox'
-    expect(settings.save_location).to be('/Dropbox')
+    expect(settings.directory).to eq '/Dropbox/jots'
   end
 end
